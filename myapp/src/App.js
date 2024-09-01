7763df8b3eb2e00d176db33fb12d4f89790c2b44
RM <rajlaxmi79812@gmail.com>
2024-09-01 22:50:58 +0530

    react practice

---
+++ myapp/src/App.js
@@ -0,0 +1,19 @@
+import Header , {hea , eader} from "./components/Header";
+// import from "./components/Header"
+import '../src/style/App.css'
+
+function App() {
+  const a = 80;
+
+  return (
+    <div>
+      <Header name ={'jead'}/>
+      {a}
+      <hea/>
+
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
