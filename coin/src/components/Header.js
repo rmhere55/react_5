ab17bcb0e41213825fbb787d671d23215bbebdc8
RM <rajlaxmi79812@gmail.com>
2024-09-07 14:11:00 +0530

    ..

--- coin/src/components/Header.js
+++ coin/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, HStack, Text } from '@chakra-ui/react';
+import {  Button, HStack, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
